bbfc8ea2f63c7d34d1f4d1499474dde1814b6d15
Onintsoa200 <77721908+Onintsoa200@users.noreply.github.com>
2022-04-06 16:47:32 +0300

    [cors]

--- sever.js
+++ sever.js
@@ -18,6 +18,13 @@ mongoose.connect('mongodb://localhost:27017/ekaly',
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
